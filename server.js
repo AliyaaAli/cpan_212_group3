7110fa0a9e4514b088a782d8a8c8b37eb8301d68
AliyaaAli <n01691766@humber.ca>
2025-12-07 17:55:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.use(
   })
 );
 
+
 // Session logging + current user
 app.use((req, res, next) => {
   console.log("Path:", req.path, "| SessionID:", req.sessionID, "| UserId:", req.session.userId);
