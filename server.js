4257b4f7cbb0220bb92b35a7666b39937c8e5d64
AliyaaAli <n01691766@humber.ca>
2025-12-07 17:53:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,6 @@ async function connectDB() {
 }
 connectDB();
 
-// Session setup
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "secret",
@@ -61,12 +60,12 @@ app.use(
       mongoUrl: process.env.MONGO_URI,
       ttl: 7 * 24 * 60 * 60, // 7 days
     }),
-   cookie: {
-  maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production", // only true in production
-  sameSite: "lax",
-},
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // only true in production
+      sameSite: "lax",
+    },
   })
 );
 
