347052d9d2e429fef9e43f01ebf63aa57778d356
AliyaaAli <n01691766@humber.ca>
2025-12-07 18:21:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // only true in production
-      sameSite: "lax",
+      sameSite: "none", // allow cookies across redirects
     },
   })
 );
