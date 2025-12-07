b309eb63488720bfc754b19a90175e6da83727d1
AliyaaAli <n01691766@humber.ca>
2025-12-06 19:46:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -56,8 +56,6 @@ app.get("/", (req, res) => res.render("home"));
 if (mongoose.connection.readyState === 0) {
   mongoose
     .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000,
     })
     .then(() => console.log("MongoDB connected"))
