a549d968de60d6f93875205b58d634ae0947e76a
AliyaaAli <n01691766@humber.ca>
2025-12-07 16:40:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -71,5 +71,13 @@ if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
-// Export for deployment
+
+const PORT = process.env.PORT || 3000;
+
+// Always start the server (Render needs this)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 module.exports = app;
+
